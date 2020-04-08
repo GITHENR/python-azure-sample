9167cd0ca923e81400e74d1d16985a85b759ad2e
GITHENR <39644606+GITHENR@users.noreply.github.com>
2020-04-08 10:56:11 +0530

    Update application.py

--- application.py
+++ application.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World! This is from Python Flask"
+    return "Hello, World! This is from Python Flask Home Page"
+
+@app.route('/about')
+def about():
+    return "Hello, this is from Python Flask about page"
