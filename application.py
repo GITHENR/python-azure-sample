918a2ed39c3276d9da30f69ea35e3b3a466f118c
GITHENR <39644606+GITHENR@users.noreply.github.com>
2020-04-09 15:03:27 +0530

    Update application.py

--- application.py
+++ application.py
@@ -12,9 +12,4 @@ def about():
 
 @app.route('/profile/<name>')
 def profile(name):
-    from azure.storage.table import TableService, Entity
-    table_service = TableService(account_name='cloudshell669196285', account_key='RVFZCljT8OJNgHFwfqwb1nig1lLHdYgPV6a2/NTF2LVFw6tpyVZdn7GZ2NU2nX1+O8A/x1fd2Q7d1ddHqYng+g==')
-    task = {'PartitionKey': 'first', 'RowKey': '004',
-        'ID': '0004', 'priority': 202,'stock':65}
-    table_service.insert_entity('customer', task)
     return render_template("Profile.html",name=name)
